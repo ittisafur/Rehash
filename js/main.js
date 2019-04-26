4d89823aa629508485c8c02f3efb3f304e2c960e
ittisafur <ittisafur@gmail.com>
2019-04-26 22:31:10 +0600

    Final Commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(function() {
+    $('.banner_images').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: true,
+        dots: false
+    })
+    $('.about-carousel').owlCarousel({
+        items: 1,
+        autoplay: true,
+    })
+
+    //Smooth Scrolling
+    $("a").on('click', function(event) {
+
+       // Make sure this.hash has a value before overriding default behavior
+       if (this.hash !== "") {
+         // Prevent default anchor click behavior
+         event.preventDefault();
+
+         // Store hash
+         var hash = this.hash;
+
+         // Using jQuery's animate() method to add smooth page scroll
+         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+         $('html, body').animate({
+           scrollTop: $(hash).offset().top
+         }, 800, function(){
+
+           // Add hash (#) to URL when done scrolling (default click behavior)
+           window.location.hash = hash;
+         });
+       } // End if
+    });
+
+
+    // Gallery
+    baguetteBox.run('.tz-gallery');
+
+    
+})
